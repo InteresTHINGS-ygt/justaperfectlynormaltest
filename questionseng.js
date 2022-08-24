8a01ca619269bfdd1fa4c6bfb6ae612420dfafe8
InteresTHINGS-ygt <69125797+InteresTHINGS-ygt@users.noreply.github.com>
2022-08-24 05:04:14 +0300

    Add files via upload

---
+++ questionseng.js
@@ -0,0 +1,182 @@
+questions = [
+    {
+        "question": "Often considered as smart by others.",
+        "effect": {
+            "res": 1.5
+        }
+    },
+    {
+        "question": "May fear sickness and accidental/early death.",
+        "effect": {
+            "res": 1.5
+        }
+    },
+    {
+        "question": "Highly caring and wants to help humanity.",
+        "effect": {
+            "res": -1.5
+        }
+    },
+   {
+        "question": "Considered as funny by others. ",
+        "effect": {
+            "res": 1.5
+        }
+    },
+   {
+        "question": "Often annoyed by cigarette smoke/smoking. ",
+        "effect": {
+            "res": 1.5
+        }
+    },
+   {
+        "question": "Uses computers and other tech well. ",
+        "effect": {
+            "res": 1.5
+        }
+    },
+   {
+        "question": "Hates being forced to do anything, has own rules.",
+        "effect": {
+            "res": 1.5
+        }
+    },
+   {
+        "question": "Neither extroverted nor introverted.  (ambivert)",
+        "effect": {
+            "res": 1.5
+        }
+    },
+   {
+        "question": "I have a good memory for the things I'm interested in. ",
+        "effect": {
+            "res": 1.5
+        }
+    },
+   {
+        "question": "I can be forgetful about things that don't interest me. ",
+        "effect": {
+            "res": 1.5
+        }
+    },
+   {
+        "question": "Has a high level of intellectual curiosity. ",
+        "effect": {
+            "res": 1.5
+        }
+    },
+   {
+        "question": "Does not need too much order to work effectively.",
+        "effect": {
+            "res": 1.5
+        }
+    },
+   {
+        "question": "Can get emotionally unstable at times. ",
+        "effect": {
+            "res": 1.5
+        }
+    },
+   {
+        "question": "Wants to be good at everything but also be a master of some. ",
+        "effect": {
+            "res": 1.5
+        }
+    },
+   {
+        "question": "Doesn't prefer soft drinks such as Coca-Cola. ",
+        "effect": {
+            "res": 1.5
+        }
+    },
+   {
+        "question": "May dash/pace around due to various reasons. ",
+        "effect": {
+            "res": 3.75
+        }
+    },
+   {
+        "question": "Fascinated by strange places/liminal spaces. ",
+        "effect": {
+            "res": 3.75
+        }
+    },
+   {
+        "question": "Cares a lot about building muscle/strength training. ",
+        "effect": {
+            "res": 3.75
+        }
+    },
+   {
+        "question": "Loves swimming and playing with water. ",
+        "effect": {
+            "res": 3.75
+        }
+    },
+   {
+        "question": "Often relates to characters, either in real life or fiction. ",
+        "effect": {
+            "res": 3.75
+        }
+    },
+   {
+        "question": "Enjoys searching for identity/personality. ",
+        "effect": {
+            "res": 3.75
+        }
+    },
+   {
+        "question": "Has high interest in digital art. ",
+        "effect": {
+            "res": 3.75
+        }
+    },
+   {
+        "question": "Gets high exam scores with a lil' bit of studying. ",
+        "effect": {
+            "res": 3.75
+        }
+    },
+   {
+        "question": "Tends to make/draw weird shapes with hands, and often accused of it.",
+        "effect": {
+            "res": 3.75
+        }
+    },
+   {
+        "question": "Likes running virtual machines/emulators. ",
+        "effect": {
+            "res": 3.75
+        }
+    },
+   {
+        "question": "Has a great time with the Rubik's Cube. (Exactly Me = Can solve it in less than a minute)",
+        "effect": {
+            "res": 8
+        }
+    },
+   {
+        "question": "Often imagines self in video games, and adds own concepts to real world in mind.",
+        "effect": {
+            "res": 8
+        }
+    },
+   {
+        "question": "Has a love for shaders and visual effects, and tends to imagine them.",
+        "effect": {
+            "res": 8
+        }
+    },
+   {
+        "question": "Great at Geometry Dash. (Exactly Me = I have beaten at least Insane levels!)",
+        "effect": {
+            "res": 8
+        }
+    },
+   {
+        "question": "Is highly interested in personality theories, such as MBTI.",
+        "effect": {
+            "res": 8
+        }
+    }
+]
---
+++ questionstur.js
@@ -0,0 +1,182 @@
+questions = [
+    {
+        "question": "Başkaları tarafından zeki olarak değerlendirilir.",
+        "effect": {
+            "res": 1.5
+        }
+    },
+    {
+        "question": "Hastalıktan ve kazara/erken ölümden korkabilir.",
+        "effect": {
+            "res": 1.5
+        }
+    },
+    {
+        "question": "İnsancıldır ve insanlığa yardım etmek ister.",
+        "effect": {
+            "res": -1.5
+        }
+    },
+   {
+        "question": "Komik ve esprili olarak görülür.",
+        "effect": {
+            "res": 1.5
+        }
+    },
+   {
+        "question": "Sigaradan ve sigara dumanından rahatsız olabilir.",
+        "effect": {
+            "res": 1.5
+        }
+    },
+   {
+        "question": "Bilgisayarları ve diğer teknolojileri iyi kullanır.",
+        "effect": {
+            "res": 1.5
+        }
+    },
+   {
+        "question": "Zorlamayı sevmez, kendi kuralları vardır.",
+        "effect": {
+            "res": 1.5
+        }
+    },
+   {
+        "question": "Ne dışadönüktür, ne de içedönüktür.",
+        "effect": {
+            "res": 1.5
+        }
+    },
+   {
+        "question": "İlgi alanlarım için iyi bir hafızam vardır.",
+        "effect": {
+            "res": 1.5
+        }
+    },
+   {
+        "question": "Bir şeyle pek ilgilenmiyorsam unutkan olabiliyorum.",
+        "effect": {
+            "res": 1.5
+        }
+    },
+   {
+        "question": "Entelektüel merakım yüksektir.",
+        "effect": {
+            "res": 1.5
+        }
+    },
+   {
+        "question": "İyi çalışmak için fazla düzene ihtiyacım yoktur.",
+        "effect": {
+            "res": 1.5
+        }
+    },
+   {
+        "question": "Bazen duygusal olarak türbülanslı olabiliyorum.",
+        "effect": {
+            "res": 1.5
+        }
+    },
+   {
+        "question": "Her şeyde iyi olmak isterim ama bazılarında uzmanlaşmak da isterim.",
+        "effect": {
+            "res": 1.5
+        }
+    },
+   {
+        "question": "Kola, Fanta gibi içecekleri tercih etmem.",
+        "effect": {
+            "res": 1.5
+        }
+    },
+   {
+        "question": "Bazı nedenlerden dolayı koşturduğum olabiliyor.",
+        "effect": {
+            "res": 3.75
+        }
+    },
+   {
+        "question": "Eşikte mekanlar ve garip yerler ilgimi çeker.",
+        "effect": {
+            "res": 3.75
+        }
+    },
+   {
+        "question": "Kas yapmak benim için çok önemlidir.",
+        "effect": {
+            "res": 3.75
+        }
+    },
+   {
+        "question": "Yüzmeyi çok severim ve suyla oynarım!",
+        "effect": {
+            "res": 3.75
+        }
+    },
+   {
+        "question": "Gerçek veya kurgusal karakterlerde kendimi görmeye çalışırım.",
+        "effect": {
+            "res": 3.75
+        }
+    },
+   {
+        "question": "Kişiliğimi bulmak bana haz verir.",
+        "effect": {
+            "res": 3.75
+        }
+    },
+   {
+        "question": "Dijital sanata ilgim vardır.",
+        "effect": {
+            "res": 3.75
+        }
+    },
+   {
+        "question": "Azıcık çalışmayla sınavda iyi puanlar alırım.",
+        "effect": {
+            "res": 3.75
+        }
+    },
+   {
+        "question": "Bazen ellerimle garip şekiller yaptığım/çizdiğim oluyor ve bu başkalarını rahatsız etmişti.",
+        "effect": {
+            "res": 3.75
+        }
+    },
+   {
+        "question": "Sanal makineler ve emülatörlere ilgim vardır.",
+        "effect": {
+            "res": 3.75
+        }
+    },
+   {
+        "question": "Rubik Küpü ile aram çok iyidir. (Tamamen Benim = 1 dakikanın altında çözerim)",
+        "effect": {
+            "res": 8
+        }
+    },
+   {
+        "question": "Kendimi bazen video oyunlarında hayal ederim, bazen kendim yarattığım konseptleri gerçekliğe uyarlıyorum.",
+        "effect": {
+            "res": 8
+        }
+    },
+   {
+        "question": "Görsel efektlere bayılırım ve hayal ederim.",
+        "effect": {
+            "res": 8
+        }
+    },
+   {
+        "question": "Geometry Dash oyununda iyiyimdir. (Tamamen Benim = en az Insane bitirmişliğim vardır)",
+        "effect": {
+            "res": 8
+        }
+    },
+   {
+        "question": "Kişilik teorilerine (Örnek: MBTI) ilgim yüksektir.",
+        "effect": {
+            "res": 8
+        }
+    }
+]
